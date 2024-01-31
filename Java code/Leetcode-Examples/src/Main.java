e1e57f62572f83af044c6ecc80b84f3505777ef5
Vikas Saini <vikassaini883@gmail.com>
2024-01-30 19:27:12 -0500

    added more problems

--- Java code/Leetcode-Examples/src/Main.java
+++ Java code/Leetcode-Examples/src/Main.java
@@ -3,9 +3,14 @@ public class Main {
     public static void main(String[] args) {
         ListNode head = new ListNode(3);
         ListNode middle = new ListNode(6);
-        ListNode tail = new ListNode(4, null);
+        ListNode tail = new ListNode(10, null);
         head.next = middle;
         middle.next = tail;
+        ListNode second = new ListNode(3);
+        ListNode m = new ListNode(8);
+        ListNode t = new ListNode(14, null);
+        second.next = m;
+        m.next = t;
         ReverseLinkedList rev = new ReverseLinkedList();
         ListNode newHead = rev.reverseList(head);
         while(newHead != null){
