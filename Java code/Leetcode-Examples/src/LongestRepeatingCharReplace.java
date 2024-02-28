730cad05f1019d5cd583c8443fe23d167adb96bc
Vikas Saini <vikassaini883@gmail.com>
2024-02-27 20:46:29 -0500

    quicksort algorithm

---
+++ Java code/Leetcode-Examples/src/LongestRepeatingCharReplace.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class LongestRepeatingCharReplace {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        int l = 0;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            frequency.compute(s.charAt(i), (key, v) -> (v == null) ? 0 :v +1);
+            int maxValue = -1;
+            for (Integer v: frequency.values()){
+                if (v > maxValue) maxValue = v;
+            }
+            while ( (i -l +1) - maxValue > k ){
+                frequency.compute(s.charAt(l), (key, v) -> (v-1));
+                l++;
+            }
+            result = Math.max(result, i-l+1);
+        }
+        return result;
+    }
+}
